72f690c8e80f60e588a8dc628758641da81a66f2
Parishlesh <parishleshfulvanshi@gmail.com>
2023-09-24 16:38:20 +0530

    the js code error is fixed and some changes in css

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
-const myDate = document.getElementById('date')
-const batteryLevel = document.getElementById('level')
+const myDate = document.getElementById('date');
+const batteryLevel = document.getElementById('level');
 
-
-myDate.innerHTML = moment().format("ddd, dd MMM, HH:mm");
-
-navigator.getBatter().then (batt=> {
-    const setBtteryLevel = ()=>{
-        const level= batt.level*100;
-
-        batteryLevel.innerText =level;
-    }
-    setBtteryLevel();
-    batt.ondischargingtimechange = ()=>{
-        setBtteryLevel();
+// Check for battery API support
+if ('getBattery' in navigator) {
+  navigator.getBattery().then(batt => {
+    const setBatteryLevel = () => {
+      const level = batt.level * 100;
+      batteryLevel.innerText = level;
     };
-    batt.onchargingtimechange = ()=>{
-        setBtteryLevel();
-    };
-})
+
+    setBatteryLevel();
+
+    batt.addEventListener('dischargingtimechange', setBatteryLevel);
+    batt.addEventListener('chargingchange', setBatteryLevel);
+  });
+} else {
+  // Handle the case where the battery API is not supported
+  batteryLevel.innerText = 'Battery API not supported';
+}
 
 setInterval(() => {
-    myDate.innerHTML = moment().format("ddd, dd MMM, HH:mm");
+  myDate.innerHTML = moment().format('ddd, DD MMM, HH:mm');
 }, 300);
+
+    // const myDate = document.getElementById('date')
+    // const batteryLevel = document.getElementById('level')
+    
+    
+    // myDate.innerHTML = moment().format("ddd, dd MMM, HH:mm");
+    
+    // navigator.getBattery().then (batt=> {
+    //     const setBtteryLevel = ()=>{
+    //         const level= batt.level*100;
+    
+    //         batteryLevel.innerText =level;
+    //     }
+    //     setBtteryLevel();
+    //     batt.ondischargingtimechange = ()=>{
+    //         setBtteryLevel();
+    //     };
+    //     batt.onchargingchange = ()=>{
+    //         setBtteryLevel();
+    //     };
+    // })
+    
+    // setInterval(() => {
+    //     myDate.innerHTML = moment().format("ddd, dd MMM, HH:mm");
+    // }, 300);
