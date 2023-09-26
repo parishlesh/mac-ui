6b4b3cd8f0812ed4f7641d344c4e551966a2ceeb
Parishlesh <parishleshfulvanshi@gmail.com>
2023-09-26 23:29:14 +0530

    script added for the date and time function

---
+++ homepage/homepage_script.js
@@ -0,0 +1,24 @@
+const myDate = document.getElementById('date');
+const batteryLevel = document.getElementById('level');
+
+// Check for battery API support
+if ('getBattery' in navigator) {
+  navigator.getBattery().then(batt => {
+    const setBatteryLevel = () => {
+      const level =Math.floor( batt.level * 100);
+      batteryLevel.innerText =level;
+    };
+
+    setBatteryLevel();
+
+    batt.addEventListener('dischargingtimechange', setBatteryLevel);
+    batt.addEventListener('chargingchange', setBatteryLevel);
+  });
+} else {
+  // Handle the case where the battery API is not supported
+  batteryLevel.innerText = 'Battery API not supported';
+}
+
+setInterval(() => {
+  myDate.innerHTML = moment().format('ddd, DD MMM, HH:mm');
+}, 300);
