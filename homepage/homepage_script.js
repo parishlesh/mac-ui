ff3352abf9c0c317844178f236122f5b08282b5a
Parishlesh <parishleshfulvanshi@gmail.com>
2023-10-01 14:47:55 +0530

    trying to adjust the dock size accordint to number of items

--- homepage/homepage_script.js
+++ homepage/homepage_script.js
@@ -1,20 +1,23 @@
-const myDate = document.getElementById('date');
-const batteryLevel = document.getElementById('level');
+document.addEventListener("DOMContentLoaded", function(){
 
-// Check for battery API support
-if ('getBattery' in navigator) {
-  navigator.getBattery().then(batt => {
-    const setBatteryLevel = () => {
-      const level =Math.floor( batt.level * 100);
-      batteryLevel.innerText =level;
-    };
-
-    setBatteryLevel();
-
-    batt.addEventListener('dischargingtimechange', setBatteryLevel);
-    batt.addEventListener('chargingchange', setBatteryLevel);
-  });
-} else {
+  
+  const myDate = document.getElementById('date');
+  const batteryLevel = document.getElementById('level');
+  
+  // Check for battery API support
+  if ('getBattery' in navigator) {
+    navigator.getBattery().then(batt => {
+      const setBatteryLevel = () => {
+        const level =Math.floor( batt.level * 100);
+        batteryLevel.innerText =level;
+      };
+      
+      setBatteryLevel();
+      
+      batt.addEventListener('dischargingtimechange', setBatteryLevel);
+      batt.addEventListener('chargingchange', setBatteryLevel);
+    });
+  } else {
   // Handle the case where the battery API is not supported
   batteryLevel.innerText = 'Battery API not supported';
 }
@@ -23,12 +26,23 @@ setInterval(() => {
   myDate.innerHTML = moment().format('ddd, DD MMM, HH:mm');
 }, 300);
 
-document.addEventListener("DOMContentLoaded", function() {
-  const dock = document.querySelectorAll('.dock');
-  const dockItems = document.querySelectorAll(".dock-items");
+const dockContainer = document.querySelector('.dock');
+const dockItems = document.querySelectorAll(".dock-item");
 
-  const dockWidth = dockItems.length;
-  
-  dock.style.width= `${dockWidth}px`;
-  console.log("working?")
+const updateDockSize = () => {
+  const numItems= dockItems.length;
+  const itemWidthPercent = 100/ numItems;
+
+  dockItems.forEach(item => {
+    item.style.width = `0 0 (${itemWidthPercent}%)`;
+  });
+
+  // dockContainer.style.width = `${numItems * itemWidthPercent}%`;
+  dockContainer.style.width = `100%`;
+};
+
+  updateDockSize();
+  window.addEventListener('resize', updateDockSize);
+  console.log("working?");
+
 });
