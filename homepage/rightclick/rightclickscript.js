3a5fea492d1d7520531dc86f4840458c7a73389a
Parishlesh <parishleshfulvanshi@gmail.com>
2023-10-07 21:21:51 +0530

    improved the code

--- homepage/rightclick/rightclickscript.js
+++ homepage/rightclick/rightclickscript.js
@@ -6,10 +6,26 @@ const contextMenu = document.getElementById('rightClickOption');
 
 desktop.addEventListener('contextmenu', (e) => {
     e.preventDefault(); // Prevent the default browser context menu
+    let menuX = e.clientX;
+    let menuY = e.clientY;
+    const menuWidth = contextMenu.offsetWidth;
+    const menuheight = contextMenu.offsetHeight;
+
+    if(menuX+menuWidth>window.innerWidth ){
+        menuX -= menuWidth;
+    }
+
+    if(menuY+menuheight>window.innerHeight ){
+        menuY -= menuheight;
+        
+    }
+    
 
     // Position the context menu at the mouse pointer
     contextMenu.style.top = e.clientY + 'px';
     contextMenu.style.left = e.clientX + 'px';
+    contextMenu.style.left = menuX + 'px';
+    contextMenu.style.top = menuY + 'px';
 
     // Show the context menu
     contextMenu.style.display = 'block';
